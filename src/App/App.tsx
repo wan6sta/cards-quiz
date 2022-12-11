67e718a2bf611ba7f1823ad78fcea64cafa7f000
wan6sta <wan6sta@gmail.com>
2022-12-11 18:18:56 +0300

    Fix App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,10 @@
 import { Storybook } from '../shared/ui/Storybook/Storybook'
+import { BugButton } from '../pages/ErrorPage/BugButton'
 
 export const App = () => {
   return (
     <div className='app'>
+      <BugButton/>
       <Storybook />
     </div>
   )
