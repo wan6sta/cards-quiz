5471789a28e8382f5f84c760f619c6d76717574d
Lecser <107482432+Lecser@users.noreply.github.com>
2022-12-11 23:45:31 +0300

    fix ErrorBoundary & StoreProvider import routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App/App'
 import './App/styles/index.css'
-import { ErrorBoundary } from './app/providers/ErrorBoundary/ErrorBoundary'
-import { StoreProvider } from './app/providers/StoreProvider/StoreProvider'
+import { ErrorBoundary } from './App/providers/ErrorBoundary/ErrorBoundary'
+import { StoreProvider } from './App/providers/StoreProvider/StoreProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StoreProvider>
