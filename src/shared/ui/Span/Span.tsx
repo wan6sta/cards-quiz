0d3205b2516c2adb760398479563574822ef571d
wan6sta <wan6sta@gmail.com>
2022-12-11 17:23:07 +0300

    Fix Span

--- src/shared/ui/Span/Span.tsx
+++ src/shared/ui/Span/Span.tsx
@@ -1,37 +1,5 @@
 import { FC, memo, PropsWithChildren } from 'react'
-import styled, { css } from 'styled-components'
-
-interface StyledSpanProps {
-  light: boolean
-  medium: boolean
-  bold: boolean
-}
-
-const StyledSpan = styled.span<StyledSpanProps>`
-  font-size: 14px;
-  line-height: 24px;
-  opacity: 0.5;
-  color: #000;
-
-  ${props =>
-    props.light &&
-    css`
-      font-weight: 400;
-    `}
-
-  ${props =>
-    props.medium &&
-    css`
-      font-weight: 600;
-    `}
-
-  ${props =>
-    props.bold &&
-    css`
-      font-weight: 500;
-      color: #000;
-    `}
-`
+import { StyledSpan } from './StyledSpan'
 
 interface SpanProps {
   light?: boolean
---
+++ src/shared/ui/Span/StyledSpan.tsx
@@ -0,0 +1,33 @@
+import styled, { css } from 'styled-components'
+
+interface StyledSpanProps {
+  light: boolean
+  medium: boolean
+  bold: boolean
+}
+
+export const StyledSpan = styled.span<StyledSpanProps>`
+  font-size: 14px;
+  line-height: 24px;
+  opacity: 0.5;
+  color: #000;
+
+  ${props =>
+    props.light &&
+    css`
+      font-weight: 400;
+    `}
+
+  ${props =>
+    props.medium &&
+    css`
+      font-weight: 600;
+    `}
+
+  ${props =>
+    props.bold &&
+    css`
+      font-weight: 500;
+      color: #000;
+    `}
+`
