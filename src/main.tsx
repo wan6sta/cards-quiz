6eb6ef1e8b9b8e8e9d36660461c1cbd0daf659c0
wan6sta <wan6sta@gmail.com>
2022-12-12 10:18:57 +0300

    Fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App/App'
+import { App } from './app/App'
 import './App/styles/index.css'
-import { ErrorBoundary } from './App/providers/ErrorBoundary/ErrorBoundary'
-import { StoreProvider } from './App/providers/StoreProvider/StoreProvider'
+import { ErrorBoundary } from './app/providers/ErrorBoundary/ErrorBoundary'
+import { StoreProvider } from './app/providers/StoreProvider/StoreProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StoreProvider>
