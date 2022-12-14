b1c1c6647883b2a4dd346992fc6c4f44f3fe2523
Mykhailo <mihasokol1993@gmail.com>
2022-12-14 08:31:31 +0200

    added route for RegistrationSuccessPage

--- src/app/providers/AppRouter/AppRouter.tsx
+++ src/app/providers/AppRouter/AppRouter.tsx
@@ -6,6 +6,7 @@ import { ErrorPage } from '../../../pages/ErrorPage/ErrorPage'
 import { PageLayout } from '../../../widgets/PageLayout/PageLayout'
 import { ProfilePage } from '../../../pages/ProfilePage/ProfilePage'
 import { RegistrationPage } from '../../../pages/RegistrationPage/RegistrationPage'
+import { RegistrationSuccessPage } from '../../../pages/RegistrationPage/RegistrationSuccessPage/RegistrationSuccessPage'
 
 const router = createBrowserRouter(
   [
@@ -38,6 +39,15 @@ const router = createBrowserRouter(
       element: (
         <PageLayout>
           <RegistrationPage />
+        </PageLayout>
+      ),
+      errorElement: <ErrorPage />
+    },
+    {
+      path: '/registrationSuccess',
+      element: (
+        <PageLayout>
+          <RegistrationSuccessPage />
         </PageLayout>
       ),
       errorElement: <ErrorPage />
