7feb8ed82a2d47eaa3ef3ecb8d69b1c7fbaccc76
Lecser <107482432+Lecser@users.noreply.github.com>
2022-12-15 12:00:24 +0300

    refactor SuccessPage

--- src/pages/RegistrationPage/RegistrationSuccessPage/RegistrationSuccessPage.tsx
+++ src/pages/RegistrationPage/RegistrationSuccessPage/RegistrationSuccessPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { BoxCard } from '../../../shared/ui/BoxCard/BoxCard'
-import { Title } from '../../../shared/ui/Title/Title'
-import { Span } from '../../../shared/ui/Span/Span'
-import { Button } from '../../../shared/ui/Button/Button'
-import { useNavigate } from 'react-router-dom'
-import { ImgWrapper } from '../../../shared/ui/ImgWrapper/ImgWrapper'
-import successPic from '../../../shared/assets/icons/registrationSuccessPic.png'
+import {BoxCard} from '../../../shared/ui/BoxCard/BoxCard'
+import {Title} from '../../../shared/ui/Title/Title'
+import {Span} from '../../../shared/ui/Span/Span'
+import {Button} from '../../../shared/ui/Button/Button'
+import {useNavigate} from 'react-router-dom'
+import {ImgWrapper} from '../../../shared/ui/ImgWrapper/ImgWrapper'
+import {ReactComponent as SuccessIcon} from '../../../shared/assets/icons/successIcon.svg';
 
 export const RegistrationSuccessPage = () => {
   const navigate = useNavigate()
@@ -16,7 +16,7 @@ export const RegistrationSuccessPage = () => {
     <BoxCard>
       <Title>Congratulations!</Title>
       <ImgWrapper>
-        <img src={successPic} alt='Icon' />
+        <SuccessIcon/>
       </ImgWrapper>
       <Span bold>Account has been created</Span>
       <Button onClick={onClickHandler} type={'button'}>
