6996bca5c20f41cb837a136ef9841a256bb633d9
wan6sta <wan6sta@gmail.com>
2022-12-22 20:54:19 +0300

    Fix

--- src/features/PacksList/lib/convertPacksData.ts
+++ src/features/PacksList/lib/convertPacksData.ts
@@ -1,5 +1,6 @@
 export const convertPacksData = <T>(userPacks: T[]): T[] => {
   return userPacks.map(card => {
+    // @ts-expect-error
     const newUpdated = card.updated.slice(0, 10).split('-').reverse().join('.')
     return { ...card, updated: newUpdated }
   })
