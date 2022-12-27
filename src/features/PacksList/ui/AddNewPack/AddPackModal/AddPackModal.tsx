839ac1089f7fcc8a7a79d1bbc2f48313c62e0acf
Lecser <107482432+Lecser@users.noreply.github.com>
2022-12-27 15:29:57 +0300

    fix AddPackModal layout

--- src/features/PacksList/ui/AddNewPack/AddPackModal/AddPackModal.tsx
+++ src/features/PacksList/ui/AddNewPack/AddPackModal/AddPackModal.tsx
@@ -1,19 +1,20 @@
 import { Flex } from '@/shared/ui/Flex/Flex'
 import { TextField } from '@/shared/ui/TextField/TextField'
 import { Checkbox } from '@/shared/ui/Checkbox/Checkbox'
+import {
+  CheckboxModalLabel,
+  TexFieldModalWrapper
+} from '@/features/PacksList/ui/AddNewPack/AddPackModal/StyledAddPackModal'
 
 export const AddPackModal = () => {
   return (
     <Flex flexDirection={'column'}>
-      <TextField
-        width={'100%'}
-        textFieldMode={'nonOutlined'}
-        title={'Name pack'}
-        margin={'0 0 30px 0'}
-      />
-      <label style={{ marginBottom: '35px' }}>
+      <TexFieldModalWrapper>
+        <TextField textFieldMode={'nonOutlined'} title={'Name pack'} />
+      </TexFieldModalWrapper>
+      <CheckboxModalLabel style={{ marginBottom: '35px' }}>
         <Checkbox label={'Private pack'} />
-      </label>
+      </CheckboxModalLabel>
     </Flex>
   )
 }
