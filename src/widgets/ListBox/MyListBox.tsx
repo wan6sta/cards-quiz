a26cabbf448b22d2b7654995fde8b9a9f7d11eef
Lecser <107482432+Lecser@users.noreply.github.com>
2022-12-29 18:27:07 +0300

    fix fix fix

--- src/widgets/ListBox/MyListBox.tsx
+++ src/widgets/ListBox/MyListBox.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import { Listbox } from '@headlessui/react'
 import { Span } from '@/shared/ui/Span/Span'
 import { Flex } from '@/shared/ui/Flex/Flex'
@@ -12,45 +12,43 @@ const options = [
   { id: 3, name: 'Video', unavailable: false }
 ]
 export const MyListBox = () => {
-  const [selectedPerson, setSelectedPerson] = useState(options[0])
+  const [selected, setSelected] = useState(options[0])
 
-  const onChangeOptionHandler = (selectedName: string) => {
-    const option = options.find(o => o.name === selectedName)
-
-    setSelectedPerson(option ? option : options[0])
-  }
   return (
-    <label className={cls.listBox}>
-      <Flex flexDirection={'column'}>
-        <Span light marginBottom={'8px'}>
-          Choose a question format
-        </Span>
-        <Listbox value={selectedPerson} onChange={setSelectedPerson}>
-          <Listbox.Button className={cls.button}>
-            {selectedPerson.name}
-            <ArrowDown />
-          </Listbox.Button>
-          <Listbox.Options className={cls.options}>
-            {options.map(option => (
-              <Listbox.Option
-                key={option.id}
-                value={option}
-                disabled={option.unavailable}
-                className={cls.default}
-              >
-                {({ active, selected }) => (
-                  <span
-                    onClick={() => onChangeOptionHandler(option.name)}
-                    className={cn(cls.default, { [cls.active]: active })}
-                  >
+    <Flex flexDirection={'column'}>
+      <Span light marginBottom={'8px'}>
+        Choose a question format
+      </Span>
+      <Listbox
+        className={cls.listBox}
+        as={'div'}
+        by={'id'}
+        value={selected}
+        onChange={setSelected}
+      >
+        <Listbox.Button className={cls.button}>
+          {selected.name}
+          <ArrowDown />
+        </Listbox.Button>
+        <Listbox.Options className={cls.options}>
+          {options.map(option => (
+            <Listbox.Option
+              key={option.id}
+              value={option}
+              disabled={option.unavailable}
+              className={cls.default}
+            >
+              {({ active }) => (
+                <>
+                  <span className={cn(cls.default, { [cls.active]: active })}>
                     {option.name}
                   </span>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Listbox>
-      </Flex>
-    </label>
+                </>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+    </Flex>
   )
 }
